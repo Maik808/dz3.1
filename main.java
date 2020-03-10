bc3f7fff6eea8fad67028ba175ff5f5255303fe2
Michail Chebaev <make7650@rambler.ru>
2020-03-10 20:10:02 +0300

    inicial commit

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+      double  ticket_price =1001.50;
+      int ticket = (int)ticket_price;
+      int result = ticket/20;
+      System.out.println(result);
+    }
+}
