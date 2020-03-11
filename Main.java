ab555464cd615b0a24846a30638357184cf3b973
Michail Chebaev <make7650@rambler.ru>
2020-03-11 18:12:27 +0300

    create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketPrice = 1001.50;
+        int ticket = (int) ticketPrice;
+        int result = ticket / 20;
+        System.out.println(result);
+    }
+}
