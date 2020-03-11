b1af5410d67716e445fda4a85324b01601aed2ca
Michail Chebaev <make7650@rambler.ru>
2020-03-11 18:00:40 +0300

    correction commit

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-      double  ticket_price =1001.50;
-      int ticket = (int)ticket_price;
-      int result = ticket/20;
-      System.out.println(result);
+        double ticketPrice = 1001.50;
+        int ticket = (int) ticketPrice;
+        int result = ticket / 20;
+        System.out.println(result);
     }
 }
